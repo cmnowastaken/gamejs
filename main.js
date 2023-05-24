4ab511b58668ced5eeb42138a4a49da0f6e1b894
Sam <94141340+cmnowastaken@users.noreply.github.com>
2023-05-25 08:50:45 +1200

    Wednesday double commit - 24/5/23

--- main.js
+++ main.js
@@ -1,25 +1,69 @@
 // Name: Εlliott Bell
-// Date: 19/5/23
+// Date: 23/5/23
 // Iteration 1
 
 let ctx;
-let CANVASWIDTH = 500;
-let CANVASHEIGHT = 500;
+const CANVASWIDTH = 500;
+const CANVASHEIGHT = 500;
+const PLAYER_WIDTH = 20;
+const PLAYER_HEIGHT = 30;
+const PROJ_WIDTH = 5;
+const PROJ_HEIGHT = 5;
+let projY = playerY;
+let projX = playerX + PLAYER_WIDTH / 2;
 let playerX = CANVASWIDTH / 2;
 let playerY = CANVASHEIGHT - 50;
 let playerColor = "#45A1E9";
 
-window.onload = startCanvas;
-
-function startCanvas() {
+const startCanvas = () => {
   ctx = document.getElementById("canvas").getContext("2d");
   console.log("canvas started");
   timer = setInterval(updateCanvas, 1000 / 60);
-}
+};
 
-updateCanvas = () => {
+const updateCanvas = () => {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, CANVASWIDTH, CANVASHEIGHT);
   ctx.fillStyle = "green";
-  ctx.fillRect(playerX, playerY, 20, 30);
+  ctx.fillRect(playerX, playerY, PLAYER_WIDTH, PLAYER_HEIGHT);
+  movePlayer();
 };
+
+const keyPressed = (keyboardEvent) => {
+  let keyPressed = keyboardEvent.key;
+  if (keyPressed == "a") {
+    leftPressed = true;
+  } else if (keyPressed == "d") {
+    rightPressed = true;
+  } else if (keyPressed == "space") {
+    drawProjectile();
+  }
+};
+
+window.addEventListener("keydown", keyPressed);
+
+const keyDepressed = (keyboardEvent) => {
+  let keyDepressed = keyboardEvent.key;
+  if (keyDepressed == "a") {
+    leftPressed = false;
+  } else if (keyDepressed == "d") {
+    rightPressed = false;
+  }
+};
+
+const movePlayer = () => {
+  const playerSpeed = Math.PI / (Math.PI / 2);
+  if (leftPressed) {
+    playerX -= playerSpeed;
+  } else if (rightPressed) {
+    playerX += playerSpeed;
+  }
+};
+
+const drawProjectile = () => {
+  ctx.fillRect(projX, projY, PROJ_WIDTH, PROJ_HEIGHT);
+};
+
+window.addEventListener("keyup", keyDepressed);
+
+startCanvas();
