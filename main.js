49476a609c67bce6120b741ff191b42a38eb3f36
Sam <94141340+cmnowastaken@users.noreply.github.com>
2023-05-26 10:41:28 +1200

    Friday - wraparound and attempts at projectiles

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Name: Εlliott Bell
 // Date: 23/5/23
-// Iteration 1
+// Iteration 2
+
+// Declaring the variables that will be used
 
 let ctx;
 const CANVASWIDTH = 500;
@@ -9,17 +11,23 @@ const PLAYER_WIDTH = 20;
 const PLAYER_HEIGHT = 30;
 const PROJ_WIDTH = 5;
 const PROJ_HEIGHT = 5;
-let projY = playerY;
-let projX = playerX + PLAYER_WIDTH / 2;
 let playerX = CANVASWIDTH / 2;
 let playerY = CANVASHEIGHT - 50;
+let projY = playerY;
+let projX = playerX + PLAYER_WIDTH / 2;
 let playerColor = "#45A1E9";
+let leftPressed;
+let rightPressed;
+
+// starting the canvas and making the framerate 60fps
 
 const startCanvas = () => {
   ctx = document.getElementById("canvas").getContext("2d");
   console.log("canvas started");
   timer = setInterval(updateCanvas, 1000 / 60);
 };
+
+// drawing the player and background 60 times/second in order to make smooth movement
 
 const updateCanvas = () => {
   ctx.fillStyle = "black";
@@ -29,18 +37,30 @@ const updateCanvas = () => {
   movePlayer();
 };
 
+const drawProjectile = () => {
+  ctx.fillStyle = "white";
+  ctx.fillRect(projX, projY, PROJ_WIDTH, PROJ_HEIGHT);
+};
+
+// logging which keys were pressed in order to move or shoot projectiles
+
 const keyPressed = (keyboardEvent) => {
   let keyPressed = keyboardEvent.key;
   if (keyPressed == "a") {
     leftPressed = true;
+    console.log(keyboardEvent.key);
   } else if (keyPressed == "d") {
     rightPressed = true;
-  } else if (keyPressed == "space") {
+    console.log(keyboardEvent.key);
+  } else if (keyPressed == " ") {
+    console.log("space");
     drawProjectile();
   }
 };
 
 window.addEventListener("keydown", keyPressed);
+
+// figuring out when keys are not pressed so that the character stops moving
 
 const keyDepressed = (keyboardEvent) => {
   let keyDepressed = keyboardEvent.key;
@@ -51,19 +71,23 @@ const keyDepressed = (keyboardEvent) => {
   }
 };
 
+window.addEventListener("keyup", keyDepressed);
+
+// taking the info from the keyPressed function and using it to move the player
+
 const movePlayer = () => {
   const playerSpeed = Math.PI / (Math.PI / 2);
   if (leftPressed) {
     playerX -= playerSpeed;
+    if (playerX < -PLAYER_WIDTH / 2) {
+      playerX = CANVASWIDTH - PLAYER_WIDTH / 2;
+    }
   } else if (rightPressed) {
     playerX += playerSpeed;
+    if (playerX > CANVASWIDTH - PLAYER_WIDTH / 2) {
+      playerX = -PLAYER_WIDTH / 2;
+    }
   }
 };
-
-const drawProjectile = () => {
-  ctx.fillRect(projX, projY, PROJ_WIDTH, PROJ_HEIGHT);
-};
-
-window.addEventListener("keyup", keyDepressed);
 
 startCanvas();
